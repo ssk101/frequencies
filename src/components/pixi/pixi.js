15d4bb669c68fd6254b51241fb57c94f875cb392
= <=>
2020-03-12 00:56:19 +0100

    more shit

--- src/components/pixi/pixi.js
+++ src/components/pixi/pixi.js
@@ -31,6 +31,101 @@ export class Pixi extends HTMLElement {
     window.addEventListener('resize', () => this.resize())
 
     this.drawScene()
+    this.initKeys()
+    this.pixi.ticker.add(delta => this.gameLoop(delta))
+  }
+
+  gameLoop(delta) {
+    this.state(delta)
+  }
+
+  play(delta) {
+    if(!this.ssky) return
+    this.ssky.x += this.ssky.vx
+    this.ssky.y += this.ssky.vy
+  }
+
+  initKeys() {
+    const dirs = {
+      up: 'ArrowUp',
+      down: 'ArrowDown',
+      left: 'ArrowLeft',
+      right: 'ArrowRight',
+    }
+    const [ w, a, s, d ] = Object.keys(dirs)
+      .map(d => {
+        var key = this.keyboard(dirs[d])
+        key.press = () => {
+          switch(d) {
+            case 'up':
+              console.log(this.ssky)
+              this.ssky.vy = -5
+              this.ssky.vx = 0
+              break
+            case 'down':
+              this.ssky.vy = 5
+              this.ssky.vx = 0
+              break
+            case 'left':
+              this.ssky.vx = -5
+              this.ssky.vy = 0
+              break
+            case 'right':
+              this.ssky.vx = 5
+              this.ssky.vy = 0
+              break
+          }
+        }
+        key.release = () => {
+
+        }
+        return key
+      })
+    this.state = this.play
+  }
+
+  keyboard(value) {
+    // TODO: unsubscribe
+    const key = {}
+    key.value = value
+    key.isDown = false
+    key.isUp = true
+    key.press = undefined
+    key.release = undefined
+    key.downHandler = event => {
+      if(event.key === key.value) {
+        if(key.isUp && key.press) key.press()
+        key.isDown = true
+        key.isUp = false
+        event.preventDefault()
+      }
+    }
+
+    key.upHandler = event => {
+      if(event.key === key.value) {
+        if(key.isDown && key.release) key.release()
+        key.isDown = false
+        key.isUp = true
+        event.preventDefault()
+      }
+    }
+
+    const downListener = key.downHandler.bind(key)
+    const upListener = key.upHandler.bind(key)
+
+    window.addEventListener(
+      'keydown', downListener, false
+    )
+    window.addEventListener(
+      'keyup', upListener, false
+    )
+
+    key.unsubscribe = () => {
+      window.removeEventListener('keydown', downListener)
+      window.removeEventListener('keyup', upListener)
+    }
+
+    return key
   }
 
   drawScene() {
@@ -44,13 +139,15 @@ export class Pixi extends HTMLElement {
         const grid = new PIXI.Sprite(
           this.pixi.loader.resources['bg_grid_static.png'].texture
         )
-        const ssky = new PIXI.Sprite(
+        this.ssky = new PIXI.Sprite(
           this.pixi.loader.resources['ssky_static.png'].texture
         )
-        ssky.x = this.clientWidth / 2
-        ssky.y = this.clientHeight / 2
+        this.ssky.x = 500
+        this.ssky.y = 500
+        this.ssky.vx = 0
+        this.ssky.vy = 0
         this.pixi.stage.addChild(grid)
-        this.pixi.stage.addChild(ssky)
+        this.pixi.stage.addChild(this.ssky)
       })
   }
 
